094a905d121365872e2564f1b345e4f70651c244
Reinhardt Velez Benedicto <rvbenedicto@addunet.ph>
2023-02-03 18:20:30 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Expense from './Expense';
 import expenses from './data';
 
+const total = expenses.reduce((total, value) => {
+  return total+value.price;}, 0)
 function App() {
-  console.log(expenses);
   return (
     <div>
       {expenses.map((expense) => {
         return(
           <Expense key={expense.id} name={expense.name} price={expense.price}/>
-        )
+          )
         })}
+        <div className='text'>
+          <h3>Total: {total}</h3>
+        </div>
     </div>
   );
 }
